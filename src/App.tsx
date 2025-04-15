8a423318b42fef396e7944c2247d82770032e2d9
Odin Kammerloher <kammerloher.andreas@gmail.com>
2025-04-15 19:59:28 +0200

    Add link to my homepage

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import CharacterSheet from "./heartsong/character_sheet/character_sheet"
 
 function App() {
     return (
-        <>
+        <div className="relative min-h-screen">
+            <a href="https://odin-matthias.de/" className="absolute top-2 left-8 text-sm underline">
+                /My Website/
+            </a>
             <div className="container mx-auto max-w-screen-xl text-red-900">
                 <CharacterSheet />
             </div>
-        </>
+        </div>
     )
 }
 
