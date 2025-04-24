7d69b95e13dd3a9c3ed82c00cc2e224a3717e29d
Odin Kammerloher <kammerloher.andreas@gmail.com>
2025-04-24 22:47:31 +0200

    Add copyright notice modal as requested

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
 import "./App.css"
 import CharacterSheet from "./heartsong/character_sheet/character_sheet"
 
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
+
 function App() {
     return (
         <div className="relative min-h-screen">
-            <a href="https://odin-matthias.de/" className="absolute top-2 left-8 text-sm underline">
-                /Odin's Site/
-            </a>
-            <a href="https://github.com/Odin94/Heartsong-Heart-character-creator" className="absolute top-2 left-32 text-sm underline">
-                /Source Code/
-            </a>
-            <a
-                href="https://rowanrookanddecard.com/product-category/game-systems/resistance/heart/"
-                className="absolute top-2 left-58 text-sm underline"
-            >
-                /Heart/
-            </a>
-            <div className="container mx-auto max-w-screen-xl text-red-900">
-                <CharacterSheet />
-            </div>
+            <Dialog>
+                <a href="https://odin-matthias.de/" target="_blank" className="absolute top-2 left-8 text-sm underline">
+                    /Odin's Site/
+                </a>
+                <a
+                    href="https://github.com/Odin94/Heartsong-Heart-character-creator"
+                    target="_blank"
+                    className="absolute top-2 left-31 text-sm underline"
+                >
+                    /Source Code/
+                </a>
+                <a
+                    href="https://rowanrookanddecard.com/product-category/game-systems/resistance/heart/"
+                    target="_blank"
+                    className="absolute top-2 left-57 text-sm underline"
+                >
+                    /Heart/
+                </a>
+
+                <DialogTrigger className="link-like absolute top-2 left-72 text-sm underline">/Copyright/</DialogTrigger>
+                <div className="container mx-auto max-w-screen-xl text-red-900">
+                    <CharacterSheet />
+                </div>
+
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>Copyright notice</DialogTitle>
+                        <div className="text-muted-foreground text-sm">
+                            <p>
+                                <em>Heartsong</em> is an independent production by <em>Odin</em> and is not affiliated with Rowan, Rook and
+                                Decard. It is published under the RR&D Community License. Heart is copyright Rowan, Rook and Decard.
+                            </p>
+                            <p className="mt-5">
+                                You can find out more and support these games at{" "}
+                                <a href="https://rowanrookanddecard.com" target="_blank">
+                                    rowanrookanddecard.com
+                                </a>
+                            </p>
+                        </div>
+                    </DialogHeader>
+                </DialogContent>
+            </Dialog>
         </div>
     )
 }
