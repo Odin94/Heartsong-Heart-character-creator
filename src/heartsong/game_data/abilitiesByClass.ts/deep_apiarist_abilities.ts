885fa7e0760b3aadafeb9a1d16d2e3e4a9554a62
Odin Kammerloher <kammerloher.andreas@gmail.com>
2025-04-27 13:14:56 +0200

    Add templates for all class abilities

---
+++ src/heartsong/game_data/abilitiesByClass.ts/deep_apiarist_abilities.ts
@@ -0,0 +1,210 @@
+import { Ability } from "../abilities"
+import { domain, skill, protection, noBonuses } from "./ability_utils"
+
+export const deep_apiarist_abilities: Ability[] = [
+    {
+        name: "___",
+        description: "___",
+        type: "minor",
+        staticBonuses: noBonuses(),
+    },
+
+    {
+        name: "___",
+        description: "___",
+        type: "minor",
+        staticBonuses: noBonuses(),
+    },
+    {
+        name: "___",
+        description: "___",
+        type: "minor",
+        staticBonuses: noBonuses(),
+    },
+    {
+        name: "___",
+        description: "___",
+        type: "minor",
+        staticBonuses: noBonuses(),
+    },
+    {
+        name: "___",
+        description: "___",
+        type: "minor",
+        staticBonuses: noBonuses(),
+    },
+    {
+        name: "___",
+        description: "___",
+        type: "minor",
+        staticBonuses: noBonuses(),
+    },
+    {
+        name: "___",
+        description: "___",
+        type: "minor",
+        staticBonuses: noBonuses(),
+    },
+    {
+        name: "___",
+        description: "___",
+        type: "minor",
+        staticBonuses: noBonuses(),
+    },
+    {
+        name: "___",
+        description: "___",
+        type: "minor",
+        staticBonuses: noBonuses(),
+    },
+    {
+        name: "___",
+        description: "___",
+        type: "minor",
+        staticBonuses: noBonuses(),
+    },
+    {
+        name: "___",
+        description: "___",
+        type: "minor",
+        staticBonuses: noBonuses(),
+    },
+    {
+        name: "___",
+        description: "___",
+        type: "minor",
+        staticBonuses: noBonuses(),
+    },
+    {
+        name: "___",
+        description: "___",
+        type: "minor",
+        staticBonuses: noBonuses(),
+    },
+    {
+        name: "___",
+        description: "___",
+        type: "minor",
+        staticBonuses: noBonuses(),
+    },
+    // Major
+    {
+        name: "___",
+        description: "___",
+        type: "minor",
+        staticBonuses: noBonuses(),
+    },
+    {
+        name: "___",
+        description: "___",
+        type: "minor",
+        staticBonuses: noBonuses(),
+    },
+    {
+        name: "___",
+        description: "___",
+        type: "minor",
+        staticBonuses: noBonuses(),
+    },
+    {
+        name: "___",
+        description: "___",
+        type: "minor",
+        staticBonuses: noBonuses(),
+    },
+
+    {
+        name: "___",
+        description: "___",
+        type: "minor",
+        staticBonuses: noBonuses(),
+    },
+    {
+        name: "___",
+        description: "___",
+        type: "minor",
+        staticBonuses: noBonuses(),
+    },
+    {
+        name: "___",
+        description: "___",
+        type: "minor",
+        staticBonuses: noBonuses(),
+    },
+    {
+        name: "___",
+        description: "___",
+        type: "minor",
+        staticBonuses: noBonuses(),
+    },
+
+    {
+        name: "___",
+        description: "___",
+        type: "minor",
+        staticBonuses: noBonuses(),
+    },
+    {
+        name: "___",
+        description: "___",
+        type: "minor",
+        staticBonuses: noBonuses(),
+    },
+    {
+        name: "___",
+        description: "___",
+        type: "minor",
+        staticBonuses: noBonuses(),
+    },
+    {
+        name: "___",
+        description: "___",
+        type: "minor",
+        staticBonuses: noBonuses(),
+    },
+
+    {
+        name: "___",
+        description: "___",
+        type: "minor",
+        staticBonuses: noBonuses(),
+    },
+    {
+        name: "___",
+        description: "___",
+        type: "minor",
+        staticBonuses: noBonuses(),
+    },
+    {
+        name: "___",
+        description: "___",
+        type: "minor",
+        staticBonuses: noBonuses(),
+    },
+    {
+        name: "___",
+        description: "___",
+        type: "minor",
+        staticBonuses: noBonuses(),
+    },
+
+    // Zenith
+    {
+        name: "___",
+        description: "___",
+        type: "minor",
+        staticBonuses: noBonuses(),
+    },
+    {
+        name: "___",
+        description: "___",
+        type: "minor",
+        staticBonuses: noBonuses(),
+    },
+    {
+        name: "___",
+        description: "___",
+        type: "minor",
+        staticBonuses: noBonuses(),
+    },
+]
